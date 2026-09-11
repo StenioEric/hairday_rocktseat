0be73d39a9ef0d804266371ec3d900cc80f0608a
StenioEric <stenioeric1@gmail.com>
2026-09-11 14:34:25 -0300

    validar nome do cliente e horário selecionado antes do envio para agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 
 const form = document.querySelector("form")
 const selectedDate = document.getElementById("date")
+const clientName = document.getElementById("client")
 
 // Date atual para input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -15,5 +16,40 @@ form.onsubmit = (event) => {
     event.preventDefault()
 
 
-    console.log("Enviado!")
+    try {
+        // Recuperando o nome do cliente.
+        const name = clientName.value.trim()
+
+        if (!name) {
+            return alert("Informe o nome do cliente!")
+        }
+
+        // Recupera o horário selecionado
+        const hourSelected = document.querySelector(".hour-selected")
+
+        // Recupera o horário selecionado
+        if (!hourSelected) {
+            return alert("Selecione a hora")
+        }
+
+        // Recupera somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+        
+        // Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        // Gera um ID
+        const id = new Date().getTime()
+
+        console.log({
+            id,
+            name,
+            when,
+        })
+
+
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
